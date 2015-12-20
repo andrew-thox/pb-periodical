b3bea72fccba9ab23735e8660f7d0c55d0c58abc
Andrew Kenyon <andrewkenyon123@gmail.com>
2015-12-20 23:33:24 +0000

    dynamic acquistion url added to scheduler

--- src/periodical/handlers.clj
+++ src/periodical/handlers.clj
@@ -1,4 +1,8 @@
-(ns periodical.handlers)
+(ns periodical.handlers
+  (:require [environ.core :refer [env]]))
+
+(def acquistion-url (clojure.string/replace "http://:host/acquisitions/:publication/:type" #":host" (env :acquistion-host)))
 
 (defn print-handler [t opts]
-  (println (:output opts) ": " t))
+  (let [url (clojure.string/replace acquistion-url #":publication|:type" {":publication" (:publication opts) ":type" (:type opts)})]
+    (println t "-" url)))
--- src/periodical/tasks.clj
+++ src/periodical/tasks.clj
@@ -1,8 +1,10 @@
 (ns periodical.tasks
-  (:require [periodical.handlers :as handlers]))
+  (:require [periodical.handlers :as handlers]
+            [environ.core :refer [env]]))
 
 (def print-task
   {:id "print-task"
    :handler handlers/print-handler
+   ;:schedule "0 /15 * * * * *"
    :schedule "/2 * * * * * *"
-   :opts {:output "Hello There"}})
+   :opts {:publication "new-statesman" :type "rss-feed"}})

c5f64e742e60fc4c53ee0949106dcab7c7bda6a1
Andrew Kenyon <andrewkenyon123@gmail.com>
2015-12-25 11:10:47 +0000

    set interval back to 15 minutes

--- src/periodical/tasks.clj
+++ src/periodical/tasks.clj
@@ -5,6 +5,6 @@
 (def acquistion-task
   {:id "new-statesman-rss-reader"
    :handler handlers/acquistion-handler
-   ;:schedule "0 /15 * * * * *"
-   :schedule "/30 * * * * * *"
+   :schedule "0 /15 * * * * *"
+   ;:schedule "/30 * * * * * *"
    :opts {:publication "new-statesman" :type "rss-feed"}})
